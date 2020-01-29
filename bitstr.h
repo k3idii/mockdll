3c1eff0e6bfdd7b1676fefb2f3efb1e8e50cac85
KeiDii <keidii@gmail.com>
2020-01-29 17:04:03 +0100

    clean stuff

---
+++ bitstr.h
@@ -0,0 +1,7 @@
+#ifdef __i386__
+#define bitstr "32"
+#endif
+
+#ifdef __x86_64__
+#define bitstr "64"
+#endif
--- build.bat
+++ build.bat
@@ -1,21 +1,24 @@
 
+
+mkdir build
+
 set VANILA_PATH=%PATH%
 
 set PATH=C:\tools\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin;%VANILA_PATH%
 
 gcc -c mock.c -o mock32.o
-gcc -shared -o mock32.dll mock32.o
+gcc -shared -o build\mock32.dll mock32.o
 gcc -c test.c -o test32.o
-gcc -o test32.exe test32.o
+gcc -o build\test32.exe test32.o
 
-test32.exe
+build\test32.exe
 
 set PATH=C:\tools\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;%VANILA_PATH%
 
 gcc -c mock.c -o mock64.o -m64
-gcc -shared -o mock64.dll mock64.o -m64
+gcc -shared -o build\mock64.dll mock64.o -m64
 gcc -c test.c -o test64.o -m64
-gcc -o test64.exe test64.o -m64
+gcc -o build\test64.exe test64.o -m64
 
-test64.exe
+build\test64.exe
 
